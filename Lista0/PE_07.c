f1609f0610eeafc5199dd4e3be5b0d2e66f01c8b
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-09-27 19:54:49 -0300

    feat: add exercises

--- Lista0/PE_07.c
+++ Lista0/PE_07.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 int main(void) {
+    float nota1;
+    float nota2;
+    float nota3;
 
+    printf("Insira a nota 1: ");
+    scanf("%f", &nota1);
+
+    printf("Insira a nota 2: ");
+    scanf("%f", &nota2);
+
+    printf("Insira a nota 3: ");
+    scanf("%f", &nota3);
+
+    float media = (nota1 + nota2 + nota3) / 3;
+
+    printf("Média: %.2f", media);
 }
