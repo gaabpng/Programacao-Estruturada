5d5c00c3176790493a125f153078aeeff86a9f93
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-09-29 21:53:06 -0300

    feat: add exercises

---
+++ Lista0/PE_10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  float temperatura;
+
+  printf("Informe a temperatura em graus Celsius: ");
+  scanf("%f", &temperatura);
+
+  float farenheit = (temperatura * 9.0 / 5) + 32;
+  printf("Temperatura em Farenheit: %.2f", farenheit);
+}
